2d4cf21078d8e127ba4c0c7249d6ef7f99859b89
eeeo0 <elly050716@gmail.com>
2025-06-02 18:12:20 +0900

    Update Enemy.java
    최신 적

--- src/shootingspaceship/Enemy.java
+++ src/shootingspaceship/Enemy.java
@@ -1,6 +1,5 @@
 package shootingspaceship; 
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.Color; 
 import javax.imageio.ImageIO; 
@@ -8,8 +7,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException; 
 import java.io.File; 
 
-public class Enemy {
-	private static Image enemyImg;
+public class Enemy { 
 
     float x_pos; 
     float y_pos; 
@@ -20,7 +18,7 @@ public class Enemy {
     float delta_y_inc; 
     final int collision_distance = 10;
     BufferedImage enemyImage; 
-    private static final String babycrocodiro_IMAGE_PATH = "src\\shootingspaceship\\image\\babycroco.png";
+    private static final String babyshark_IMAGE_PATH = "src\\shootingspaceship\\image\\babyshark_64x64.png";
     //스테이지마다 적과 보스의 이미지가 달라져야하는데.... 모르겟음
     // 적 생성자: 위치, 속도, 화면 크기, 속도 증가량을 받아 초기화
     public Enemy(int x, int y, float delta_x, float delta_y, int max_x, int max_y, float delta_y_inc) {
@@ -31,15 +29,13 @@ public class Enemy {
         this.max_x = max_x; 
         this.max_y = max_y; 
         this.delta_y_inc = delta_y_inc; 
-        
         try {
-        	enemyImage = ImageIO.read(new File(babycrocodiro_IMAGE_PATH));
-        } catch (IOException e) {
-            e.printStackTrace();
+            enemyImage = ImageIO.read(new File(babyshark_IMAGE_PATH)); 
+        } catch(IOException e) {
+            e.printStackTrace(); 
         }
-    
     }
-    
+
     // 적 이동 메서드
     public void move() {
         x_pos += delta_x; 
@@ -58,11 +54,10 @@ public class Enemy {
         // }
         //지금은 바닥에 닿으면 게임오버처리.
     }
-    
+    //추가기능
     public float getX() {
-        return x_pos;
+    	return x_pos;
     }
-
     public float getY() {
         return y_pos;
     }
@@ -76,7 +71,6 @@ public class Enemy {
                 if (-collision_distance <= (x_pos - shot.getX()) && (x_pos - shot.getX() <= collision_distance)) {
                     shot.collided(); 
                     return true; 
-
                 }
             }
         }
@@ -85,20 +79,16 @@ public class Enemy {
 
     // 적이 플레이어와 충돌했는지 검사
     public boolean isCollidedWithPlayer(Player player) {
-    	Rectangle bombBounds = getBounds();
-        Rectangle playerBounds = player.getBounds();
-    	
-    	int collision_distance = 32;
         if (-collision_distance <= (y_pos - player.getY()) && (y_pos - player.getY() <= collision_distance)) {
             if (-collision_distance <= (x_pos - player.getX()) && (x_pos - player.getX() <= collision_distance)) {
-            	System.out.println("폭탄과 플레이어 충돌!");
                 return true; 
             }
         }
         return false; 
     }
+
     public void draw(Graphics g) {
-        if(enemyImage != null) {
+        if(enemyImage != null) { 
             int imgW = enemyImage.getWidth(); 
             int imgH = enemyImage.getHeight(); 
             g.drawImage(enemyImage, (int)(x_pos - imgW/2),(int)(y_pos - imgH /2),null);
@@ -109,7 +99,7 @@ public class Enemy {
             g.fillPolygon(x_poly, y_poly, 4); 
             g.drawImage(enemyImage, max_x, max_y, null); 
         }
-  }
+    }
 
 	public Rectangle getBounds() {
 		// TODO Auto-generated method stub
