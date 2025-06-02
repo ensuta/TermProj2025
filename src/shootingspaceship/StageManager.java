7b3c7edc84e37182e514e01beae4c9a2632774d9
eunbino <bin5053@gmail.com>
2025-06-03 03:52:49 +0900

    stage 5 -> 4

--- src/shootingspaceship/StageManager.java
+++ src/shootingspaceship/StageManager.java
@@ -10,12 +10,12 @@ import java.awt.Image;
 public class StageManager {
     //스테이지 설정
     private int currentStage = 1; 
-    private int maxStage = 5;
+    private int maxStage = 4;
 
     // 각 스테이지별 설정값
-    private final int[] enemiesPerStage = {5, 10, 15, 20, 25}; 
-    private final int[] bossHealthPerStage = {20, 30, 40, 50, 60}; //스테이지 별 보스 피 
-    private final float[] bossSpeedPerStage = {0.2f, 0.4f, 0.6f, 0.7f, 0.8f}; //스테이지 별 보스 속도
+    private final int[] enemiesPerStage = {5, 10, 15, 20}; 
+    private final int[] bossHealthPerStage = {20, 30, 40, 50}; //스테이지 별 보스 피 
+    private final float[] bossSpeedPerStage = {0.2f, 0.4f, 0.6f, 0.8f}; //스테이지 별 보스 속도
     
     //배경 이미지
     private Image backgroundImage;
